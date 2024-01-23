b8ba92e7b0d6045e1a30ca871e1a96732be11e76
Mirza Ardani <mirza@doku.com>
2024-01-23 09:38:50 +0700

    Rename Jokul to Doku

--- Jokul/Magento2/Controller/Payment/RequestO2O.php
+++ Jokul/Magento2/Controller/Payment/RequestO2O.php
@@ -153,7 +153,7 @@ class RequestO2O extends \Magento\Framework\App\Action\Action
                     "additional_info" => array (
                         "integration" => array (
                             "name" => "magento-plugin",
-                            "version" => "1.4.5",
+                            "version" => "1.4.6",
                             "cms_version" => $productMetadata->getVersion()
                         ),
                         "method" => "Jokul Direct",
@@ -185,7 +185,7 @@ class RequestO2O extends \Magento\Framework\App\Action\Action
                     "additional_info" => array (
                         "integration" => array (
                             "name" => "magento-plugin",
-                            "version" => "1.4.5",
+                            "version" => "1.4.6",
                             "cms_version" => $productMetadata->getVersion()
                         ),
                         "method" => "Jokul Direct"
